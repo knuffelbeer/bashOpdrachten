51b472ff62979420a7e85675d1cf7a2dd7a109b8
knuffelbeer <beerforbusiness@outlook.com>
2024-10-08 23:47:12 +0200

    Removed files not present in local repository

--- converteerUnderscores.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-bestanden=$(ls data/)
-for bestand in $bestanden; do
-	nieuweNaam=$(echo "$bestand" | sed 's/_/ /g')
-	echo $nieuweNaam
-	mv "data/$bestand" "data/$nieuweNaam"
-done
--- generateFiles.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while read -r p; do # een manier om door een bestand heen te loopen
-				name=$(echo "$p" | awk 'BEGIN {FS=","} {print $2}') # selecteer het tweede veld (komma-gescheiden)
-	name=$(echo "$name" | awk 'BEGIN {FS=" "} {print $1 "_" $2}') # scheid de voor en achternaam met een _
-	echo "$p" > "data/$name.txt" # Schrijf de regel naar een bestand
-done < "data.txt"
-
