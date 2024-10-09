d43e95defd6f7361289db8f9dd998b7f38e87b59
knuffelbeer <beerforbusiness@outlook.com>
2024-10-09 07:55:46 +0200

    test

---
+++ voorbeeldAntwoorden/git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $1 == "" ]]; then 
+	echo "submit message!"
+	read -r message 
+	if [[ $message == "" ]]; then
+	message="Enter a message:"
+  read -r message 
+  if [[ $message == "" ]]; then
+					message="No message"
+  fi
+	fi
+else 
+	message=$1
+fi
+git add .
+git commit -m "$message"
+echo "You sure?! (press y)"
+read -r confirm 
+if [[ $confirm == "y" ]]; then
+				git push
+else 
+	echo 'no push was made'
+fi
