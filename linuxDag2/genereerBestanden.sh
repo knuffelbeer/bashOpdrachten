73e41adfbdd9c660b8b3d52414449fc07f277b89
knuffelbeer <beerforbusiness@outlook.com>
2025-01-13 14:05:53 +0100

    genereer bestanden

--- linuxDag2/genereerBestanden.sh
+++ linuxDag2/genereerBestanden.sh
@@ -2,11 +2,13 @@
 
 if ! find . data -type d 2> /dev/null > /dev/null; then 
 	mkdir data
+	echo "created data directory"
 fi
 
-while read -r p; do # een manier om door een bestand heen te loopen
-				name=$(echo "$p" | awk 'BEGIN {FS=","} {print $2}') # selecteer het tweede veld (komma-gescheiden)
-	name=$(echo "$name" | awk 'BEGIN {FS=" "} {print $1 "_" $2}') # scheid de voor en achternaam met een _
-	echo "$p" > "data/$name.txt" # Schrijf de regel naar een bestand
-done < "data.txt"
-
+tail -n +2 "data.txt" | while read -r i; do
+				name=$(echo "$i" | awk 'BEGIN {FS=","} {print $2}') # selecteer het tweede veld (komma-gescheiden)
+	      name=$(echo "$name" | awk 'BEGIN {FS=" "} {print $1 "_" $2}') # scheid de voor en achternaam met een _
+				echo "$i" > "data/$name.txt" # Schrijf de regel naar een bestand
+				echo "created file data/$name.txt"
+done
+echo "Done!"
